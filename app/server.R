5185bbdd38606504e00d458d4ac3da1fb4df9111
Jill MacKay <jmackay4@ed.ac.uk>
2023-09-21 16:59:25 +0100

    add random data for example

--- app/server.R
+++ app/server.R
@@ -30,10 +30,15 @@ shinyServer(function(input, output, session){
   })
 
 
+  exdat <- tibble(grades = round(rnorm(100, 55, 20),0))
 
 
   hofs <- reactive({
     hofstee(dat()$marks, input$i_low.fail.pc, input$i_high.fail.pc, input$i_low.mark, input$i_high.mark )
+  })
+
+  exhofs <- reactive({
+    hofstee(exdat$grades, input$ex_lowfails, input$ex_highfails, input$ex_lowmark, input$ex_highmark)
   })
 
 
@@ -57,6 +62,24 @@ shinyServer(function(input, output, session){
     hof_plot(dat()$marks,  input$i_low.fail.pc, input$i_high.fail.pc, input$i_low.mark, input$i_high.mark)
   })
 
+  output$p_exdat <- renderPlot({
+
+    exdat |>
+      ggplot(aes(x = row_number(grades))) +
+      geom_point(aes(y = grades), position = position_jitter(width = .13), size = 0.5, alpha = 0.6) +
+      see::geom_violinhalf(aes(y = grades, alpha= 0.3), linetype = "dashed", position = position_nudge(x = .2)) +
+      geom_boxplot(aes(y = grades, alpha = 0.3), position = position_nudge(x = -.1), width = 0.1, outlier.shape = NA) +
+      theme_classic() +
+      labs(x = "Feed Type", y = "Grades") +
+      theme(legend.position = "none") +
+      coord_flip()
+  })
+
+
+  output$p_exhofplot <- renderPlot({
+
+    hof_plot(exdat$grades, input$ex_lowfails, input$ex_highfails, input$ex_lowmark, input$ex_highmark)
+  })
 
   # ------------- Return Tables
 
@@ -64,6 +87,12 @@ shinyServer(function(input, output, session){
   output$t_hofstable <- renderTable({
     hof_table(dat()$marks,  input$i_low.fail.pc, input$i_high.fail.pc, input$i_low.mark, input$i_high.mark)
   })
+
+
+  output$t_exhofstable <- renderTable({
+    hof_table(exdat$grades, input$ex_lowfails, input$ex_highfails, input$ex_lowmark, input$ex_highmark)
+  })
+
 
 
  #------------ Close server bracket
--- app/ui.R
+++ app/ui.R
@@ -4,29 +4,61 @@ library(DT)
 
 
 
-shinyUI(pageWithSidebar(
-  headerPanel("Hofstee Standard Setting"),
-  sidebarPanel(tags$p("Upload your marks file here. The only data required is the final course mark – this should be in a column headed ‘marks’ and saved as a", tags$em(".xlsx"), "file. There is no need to have a specific name for the file."),
-               fileInput(inputId = "grade_file",
-                         label = "Upload your xlsx file here"),
-               textInput(inputId = "course_name",
-                         label = "You can add the course name here",
-                         placeholder = "ICC Farm"),
-               numericInput(inputId = "i_low.fail.pc",
-                            label = "The lowest acceptable % of students to fail (Default 0%)",
-                            value = 0),
-               numericInput(inputId = "i_high.fail.pc",
-                            label = "The highest acceptable % of students to fail (Default 10%)",
-                            value = 10),
-               numericInput(inputId = "i_low.mark",
-                            label = "Lowest acceptable % correct score to allow borderline student to pass (Default 48%)",
-                            value = 48),
-               numericInput(inputId = "i_high.mark",
-                            label = "Highest acceptable % correct score to allow borderline student to pass (Default 52%)",
-                            value = 52)),
-  mainPanel(tableOutput(outputId = "t_hofstable"),
-    plotOutput(outputId = "p_hofplot"))
-)
+
+shinyUI(
+  navbarPage(title = "Hofstee Standard Setting",
+             id = "navbar",
+             theme = shinythemes::shinytheme("yeti"),
+
+             tabPanel(title = "Explore Standard Setting",
+                      sidebarPanel(
+                        sliderInput(inputId = "ex_lowfails",
+                                    label = "The lowest acceptable % of students to fail:",
+                                    min = 0,
+                                    max = 100,
+                                    value = 0),
+                        sliderInput(inputId = "ex_highfails",
+                                    label = "The highest acceptable % of students to fail:",
+                                    min = 0,
+                                    max = 100,
+                                    value = 10),
+                        sliderInput(inputId = "ex_lowmark",
+                                    label = "The lowest acceptable % correct score to allow borderline student to pass:",
+                                    min = 0,
+                                    max = 100,
+                                    value = 48),
+                        sliderInput(inputId = "ex_highmark",
+                                    label = "The highest acceptable % correct score to allow borderline student to pass:",
+                                    min = 0,
+                                    max = 100,
+                                    value = 52)
+                      ),
+                      mainPanel(plotOutput(outputId = "p_exdat"),
+                                tableOutput(outputId = "t_exhofstable"),
+                                plotOutput(outputId = "p_exhofplot"))),
+             tabPanel(title = "Standard set your own marks",
+                      sidebarPanel(tags$p("Upload your marks file here. The only data required is the final course mark – this should be in a column headed ‘marks’ and saved as a", tags$em(".xlsx"), "file. There is no need to have a specific name for the file."),
+                                   fileInput(inputId = "grade_file",
+                                             label = "Upload your xlsx file here"),
+                                   textInput(inputId = "course_name",
+                                             label = "You can add the course name here",
+                                             placeholder = "ICC Farm"),
+                                   numericInput(inputId = "i_low.fail.pc",
+                                                label = "The lowest acceptable % of students to fail (Default 0%)",
+                                                value = 0),
+                                   numericInput(inputId = "i_high.fail.pc",
+                                                label = "The highest acceptable % of students to fail (Default 10%)",
+                                                value = 10),
+                                   numericInput(inputId = "i_low.mark",
+                                                label = "Lowest acceptable % correct score to allow borderline student to pass (Default 48%)",
+                                                value = 48),
+                                   numericInput(inputId = "i_high.mark",
+                                                label = "Highest acceptable % correct score to allow borderline student to pass (Default 52%)",
+                                                value = 52)),
+                      mainPanel(tableOutput(outputId = "t_hofstable"),
+                                plotOutput(outputId = "p_hofplot"))
+                      )
+             )
 )
 
 
